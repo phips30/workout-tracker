4d27b41d65dcfd60dae0c23df264b3f199a40d88
phips30 <wiesinger.philipp@gmail.com>
2025-06-03 22:03:39 +0200

    Initial workout controller to receive completed workouts

---
+++ src/main/java/com/workouttracker/tracker/workouts/rest/WorkoutController.java
@@ -0,0 +1,17 @@
+package com.workouttracker.tracker.workouts.rest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/workouts")
+public class WorkoutController {
+
+    @PostMapping("{id}")
+    public ResponseEntity<Void> addNewWorkout(@PathVariable UUID id, @RequestBody Object workout) {
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+}

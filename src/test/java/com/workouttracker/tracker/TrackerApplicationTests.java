19af30a7e2c852353d3f95f69cd93b0620993a88
phips30 <wiesinger.philipp@gmail.com>
2025-06-03 21:46:24 +0200

    Initial commit

---
+++ src/test/java/com/workouttracker/tracker/TrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workouttracker.tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

600fffaa3b26d30a0cf7d4c4dabe934864fd5176
phips30 <wiesinger.philipp@gmail.com>
2025-08-29 20:32:27 +0200

    Add global exception handler draft

---
+++ src/main/java/com/phips30/workouttracker/workout/infrastructure/rest/WorkoutTrackerExceptionHandler.java
@@ -0,0 +1,56 @@
+package com.phips30.workouttracker.workout.infrastructure.rest;
+
+import com.phips30.workouttracker.workout.domain.usecase.RoutineAlreadyExistsException;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.Date;
+
+@ControllerAdvice
+public class WorkoutTrackerExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @Getter
+    @Setter
+    class ErrorBody {
+        Date dateTime;
+        String message;
+
+        public ErrorBody(String message) {
+            this.dateTime = new Date();
+            this.message = message;
+        }
+
+        public Date getDateTime() {
+            return dateTime;
+        }
+
+        public void setDateTime(Date dateTime) {
+            this.dateTime = dateTime;
+        }
+
+        public String getMessage() {
+            return message;
+        }
+
+        public void setMessage(String message) {
+            this.message = message;
+        }
+    }
+
+    @ExceptionHandler({
+            RoutineAlreadyExistsException.class,
+    })
+    ResponseEntity<Object> handleObjectExists(Exception ex, WebRequest request) {
+        String bodyOfResponse = "This should be application specific";
+        return super.handleExceptionInternal(ex, new ErrorBody(ex.getMessage()),
+                new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
+    }
+}

02b04d7c815a0226b63d73b278fbdc85cd7b8d7d
phips30 <wiesinger.philipp@gmail.com>
2025-09-01 21:17:32 +0200

    WorkoutController Stub

--- src/main/java/com/phips30/workouttracker/workout/infrastructure/rest/WorkoutController.java
+++ src/main/java/com/phips30/workouttracker/workout/infrastructure/rest/WorkoutController.java
@@ -1,17 +1,23 @@
 package com.phips30.workouttracker.workout.infrastructure.rest;
 
+import com.phips30.workouttracker.workout.domain.entity.Workout;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.UUID;
+import java.util.List;
 
 @RestController
-@RequestMapping("/api/workouts")
+@RequestMapping("/api/workout")
 public class WorkoutController {
 
-    @PostMapping("{id}")
-    public ResponseEntity<Void> addNewWorkout(@PathVariable UUID id, @RequestBody Object workout) {
+    @PostMapping("{routineName}")
+    public ResponseEntity<Void> addNewWorkout(@PathVariable String routineName, @RequestBody Workout workout) {
         return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @GetMapping("{routineName}")
+    public ResponseEntity<List<Workout>> getWorkouts(@PathVariable String routineName) {
+        return ResponseEntity.ok(null);
     }
 }

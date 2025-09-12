10a06f410fef45f80c4c27798eff69aa2e4e2817
phips30 <wiesinger.philipp@gmail.com>
2025-09-12 21:20:36 +0200

    Implement exercisename

--- src/main/java/com/phips30/workouttracker/workout/domain/valueobjects/ExerciseName.java
+++ src/main/java/com/phips30/workouttracker/workout/domain/valueobjects/ExerciseName.java
@@ -1,11 +1,39 @@
 package com.phips30.workouttracker.workout.domain.valueobjects;
 
-public class ExerciseName {
-    public ExerciseName(String name) {
+import java.util.Objects;
 
+public class ExerciseName {
+
+    private final String value;
+
+    public ExerciseName(String value) {
+        if (value == null || value.trim().isEmpty()) {
+            throw new IllegalArgumentException("Exercise name cannot be null or empty");
+        }
+        if (value.trim().length() <= 2) {
+            throw new IllegalArgumentException("Exercise name cannot be less than three characters");
+        }
+        this.value = value;
     }
 
     public String getValue() {
-        return null;
+        return this.value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof ExerciseName otherName)) return false;
+        return this.value.equals(otherName.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.value);
+    }
+
+    @Override
+    public String toString() {
+        return this.value;
     }
 }

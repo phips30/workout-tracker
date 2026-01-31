f2c44d96c5601aebfe8c83decedcb8a518f39967
phips30 <wiesinger.philipp@gmail.com>
2026-01-31 21:32:25 +0100

    finish exercise integration tests #2

--- src/main/java/com/phips30/workouttracker/workout/infrastructure/rest/ExerciseController.java
+++ src/main/java/com/phips30/workouttracker/workout/infrastructure/rest/ExerciseController.java
@@ -6,16 +6,20 @@ import com.phips30.workouttracker.workout.domain.usecase.ExerciseService;
 import com.phips30.workouttracker.workout.infrastructure.rest.dto.ExerciseResponse;
 import com.phips30.workouttracker.workout.infrastructure.rest.dto.NewExerciseRequest;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static com.phips30.workouttracker.workout.infrastructure.rest.ExerciseController.BASE_PATH;
+
 @RestController
-@RequestMapping("/api/exercise")
+@RequestMapping(BASE_PATH)
 public class ExerciseController {
+
+    protected static final String BASE_PATH = "/api/exercise";
 
     private final ExerciseService exerciseService;
 
@@ -34,8 +38,7 @@ public class ExerciseController {
     @PostMapping
     public ResponseEntity<ExerciseResponse> addExercise(@RequestBody NewExerciseRequest exerciseRequest) throws ExerciseAlreadyExistsException {
         Exercise exercise = exerciseService.create(exerciseRequest.name());
-        return new ResponseEntity<>(
-                new ExerciseResponse(exercise.getId().getId().toString(), exercise.getName().getValue()),
-                HttpStatus.CREATED);
+        return ResponseEntity.created(URI.create(BASE_PATH))
+                .body(new ExerciseResponse(exercise.getId().getId().toString(), exercise.getName().getValue()));
     }
 }
--- src/test/java/com/phips30/workouttracker/workout/infrastructure/rest/ExerciseControllerIT.java
+++ src/test/java/com/phips30/workouttracker/workout/infrastructure/rest/ExerciseControllerIT.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -26,11 +27,14 @@ class ExerciseControllerIT {
     @Test
     public void addExercise_doesNotExist_addedToDatabase() {
         NewExerciseRequest exercise = new NewExerciseRequest(randomString(10));
-        ResponseEntity<Void> postReponse = restTemplate.postForEntity("/api/exercise", exercise, Void.class);
+        ResponseEntity<ExerciseResponse> postReponse = restTemplate.postForEntity("/api/exercise", exercise, ExerciseResponse.class);
         assertThat(postReponse.getStatusCode()).isEqualTo(HttpStatus.CREATED);
 
         ResponseEntity<List<ExerciseResponse>> getResponse = restTemplate
-                .exchange("/api/exercise", HttpMethod.GET, null, new ParameterizedTypeReference<>() {});
+                .exchange(postReponse.getHeaders().get(HttpHeaders.LOCATION).getFirst(),
+                        HttpMethod.GET,
+                        null,
+                        new ParameterizedTypeReference<>() {});
 
         assertThat(getResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
         assertTrue(getResponse.getBody().stream().anyMatch(e -> exercise.name().equals(e.name())));
